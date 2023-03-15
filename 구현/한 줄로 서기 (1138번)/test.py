e60787a0fdcece1aea7a40395cc4b05da91e9611
alg0r1thmm <jeongsw1217@gmail.com>
2023-03-15 17:44:47 +0900

    230315 한 줄로 서기 (1138번)

---
+++ 구현/한 줄로 서기 (1138번)/test.py
@@ -0,0 +1,15 @@
+n = int(input())
+heights = list(map(int, input().split()))
+
+line = [0] * n  
+for i in range(n):
+    # 현재 사람(i번째)보다 키가 큰 사람의 수
+    count = 0  
+    for j in range(n):
+        if count == heights[i] and line[j] == 0:
+            line[j] = i + 1
+            break
+        if line[j] == 0 or line[j] > i + 1:
+            count += 1
+
+print(*line)  
